28401a8ac07d714c935daf8e302baf56373f7c72
Isaque Rosa <isaque@darwinsoft.com>
2023-06-28 20:55:05 -0300

    Base inicial do projeto

---
+++ src/styles/theme/index.tsx
@@ -0,0 +1,32 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+    GREEN_LIGHT: '#00B37E',
+    GREEN: '#00875F',
+    GRAY_100: '#ecf2f7',
+    GRAY_200: '#C4C4CC',
+    GRAY_300: '#8D8D99',
+    GRAY_400: '#7b8389',
+    GRAY_500: '#616161',
+    GRAY_600: '#323238',
+    GRAY_700: '#29292E',
+    GRAY_800: '#202024',
+    BLUE: '#2e77d1',
+    BLUE_LIGHT: '#66bce8',
+    BLUE_DARK: '#255fa7',
+    ERROR: '#e35b5c',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Poppins_500Medium',
+    BOLD: 'Poppins_700Bold',
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 20,
+    XXL: 24,
+    XXXL: 32,
+  },
+}
